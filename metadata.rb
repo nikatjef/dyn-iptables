3ff3746bb7a8856e9a82c2b40bb8e2fa753bbd6f
Sean OMeara <someara@opscode.com>
2013-09-07 23:15:59 -0400

    updating to 0.2.2

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'Sean OMeara <someara@opscode.com>'
 license          'Apache2'
 description      'Installs/Configures iptables'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.1'
+version          '0.2.2'
 
 depends "partial_search"
