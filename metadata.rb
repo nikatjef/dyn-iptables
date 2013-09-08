ac5f1482711207cff5cfb684ababa5b8279faaed
Sean OMeara <someara@opscode.com>
2013-09-07 23:13:01 -0400

    bumping to 0.2.1

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'Sean OMeara <someara@opscode.com>'
 license          'Apache2'
 description      'Installs/Configures iptables'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.0'
+version          '0.2.1'
 
 depends "partial_search"
